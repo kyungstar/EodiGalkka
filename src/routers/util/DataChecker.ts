aed2e85a828ac834eaed117e8604bf4644e6d775
kyungstar <azure0563@gmail.com>
2023-07-02 11:07:48 +0900

    클라이언트에서 null로 줬을 경우, enum  type 대비 수정완료

--- src/routers/util/DataChecker.ts
+++ src/routers/util/DataChecker.ts
@@ -6,96 +6,6 @@ import Config from "../../../config";
 
 class DataChecker extends ResController {
 
-    // 폐기 예정
-    public numberArrCheck(res: any, objData: any, numberArr: string[], defaultValue: number, isRequire: boolean) {
-
-        let retObj = {};
-        let dataFailList: string[] = [];
-
-        for (let item of numberArr) {
-
-            if ((objData[item] == '' || objData[item] == undefined) && isRequire === true)
-                dataFailList.push(item)
-
-
-            if ((objData[item] == '' || objData[item] == undefined) && isRequire === false)
-                objData[item] = 1;
-
-            // @ts-ignore
-            retObj[item] = parseInt(objData[item]);
-
-        }
-
-        if (dataFailList.length > 0) {
-            return dataFailList + ' Is Essential Data';
-        }
-
-
-        return retObj;
-    }
-
-    // 필수 값 검증
-    public needArrCheck(res: any, objData: any, needArr: string[]) {
-
-
-        try {
-
-            let retObj = {};
-            let dataFailList = [];
-
-
-            for (let item of needArr) {
-
-
-                if (objData[item] === undefined || objData[item] === "") {
-                    dataFailList.push(item)
-                }
-
-                // @ts-ignore
-                retObj[item] = objData[item];
-
-            }
-
-            if (dataFailList.length > 0) {
-                return dataFailList + ' Is Essential Data';
-            }
-
-
-            return retObj;
-
-        } catch (err) {
-            return err;
-        }
-
-    }
-
-
-    public stringArrCheck(res: any, objData: any, strArr: string[], isRequire: boolean) {
-
-        let retObj = {};
-        let dataFailList = [];
-
-        for (let item of strArr) {
-
-            if ((objData[item] == '' || objData[item] == undefined) && isRequire === true)
-                dataFailList.push(item)
-
-            // @ts-ignore
-            retObj[item] = objData[item];
-
-        }
-
-        if (dataFailList.length > 0)
-            return dataFailList + ' Is Essential Data';
-
-
-        return retObj;
-
-    }
-
-
-    //------------------------------------------------------------
-
 
     // 토큰 꺼내기
     public loadJWTValue(objData: any) {
@@ -152,7 +62,7 @@ class DataChecker extends ResController {
 
         for (const item of strNeedArr) {
 
-            if ((objData[item] === '' || objData[item] === undefined))
+            if ((objData[item] === '' || objData[item] === undefined || objData[item] === 'null'))
                 dataFailList.push(item);
 
             retObj[item] = objData[item].toString();
