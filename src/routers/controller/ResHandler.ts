83dda8085e0a30cd46c580c06b37f53173104d52
kyungstar <azure0563@gmail.com>
2023-10-11 00:21:53 +0900

    ResHandler 어느정도 완성되었음.. 추후 Object 수정 될 가능성이 농후함..

---
+++ src/routers/controller/ResHandler.ts
@@ -0,0 +1,74 @@
+import express, {Response} from "express";
+
+import Logger from "../../modules/middlewares/Logger";
+import {MyAuth} from "../../modules/middlewares/SecurityAuth";
+
+
+export default class ResHandler {
+
+
+      validErr(res: express.Response) {
+
+        const response = {
+            success: false,
+            message: MyAuth.getEncryptCode("데이터 검증에 실패하였습니다.")
+        };
+
+        Logger.info("API Response : " + JSON.stringify({
+            success: false,
+            message: "데이터 검증에 실패하였습니다."
+        }));
+
+        return res.status(200).json(response);
+    }
+
+
+    false(code: string, res: express.Response) {
+
+        const response = {
+            success: false,
+            message: MyAuth.getEncryptCode(code)
+        };
+
+        Logger.info("API Response : " + JSON.stringify({
+            success: false,
+            message: code
+        }));
+
+        return res.status(200).json(response);
+
+    }
+
+
+     true(code: string, res: Response) {
+
+        const response = {
+            success: false,
+            message: MyAuth.getEncryptCode(code)
+        };
+
+        Logger.info("API Response : " + JSON.stringify({
+            success: false,
+            message: code
+        }));
+
+        return res.status(200).json(response);
+
+    }
+
+
+    err(err: any, res: express.Response) {
+
+        Logger.error(err + ' is Occurred');
+
+        const response = {
+            success: false,
+            err: err
+        };
+
+        return res.status(200).json(response);
+
+    }
+
+
+}
