cdb784bab72b1482e61b2140c80cc88c2d32e299
kyungstar <azure0563@gmail.com>
2023-10-18 01:25:27 +0900

    고객 로그인 및 프로필 작업

--- src/routers/api/UserRoutes.ts
+++ src/routers/api/UserRoutes.ts
@@ -1,12 +1,21 @@
-import {Router} from "express";
+import {json, Router} from "express";
 
-import UserController from '../controller/User/UserController'
+import UserController from '../controller/User/UserController';
+import {JwtCheck} from "../../modules/middlewares/SecurityAuth"
+
 
 const router = Router();
 
 
 // 회원가입
 router.post("/join", UserController.userJoin);
+
+// 고객 로그인
+router.post("/login", UserController.userLogin);
+
+// 고객 프로필
+router.post("/profile", [JwtCheck], UserController.userProfile);
+
 
 
 export default router;
