76e3292c7a4b51d288facf1930c0d54096792820
kyungstar <azure0563@gmail.com>
2023-10-29 23:14:53 +0900

    FileRoutes 분리

--- src/routers/api/FileRoutes.ts
+++ src/routers/api/FileRoutes.ts
@@ -10,15 +10,15 @@ const router = Router();
 // 사진 업로드 > 파일 타입 추가하기
 router.post("/img/upload", [JwtCheck], FileController.imgUpload);
 
-// 사진 다운로드
-router.get("/img/download", [JwtCheck], FileController.imgDownload);
-
-// 파일 경로 획득하기
-
 // 영상 업로드
 router.post("/mov/upload", [JwtCheck], FileController.movUpload);
 
-// 영상다운로드
-router.get("/mov/download", [JwtCheck], FileController.movUpload);
+// Seq로 다운로드
+router.get("/downloadSeq", [JwtCheck], FileController.imgDownloadBySeq);
+
+// 경로로 파일 다운로드
+router.get("/downloadPath", [JwtCheck], FileController.imgDownloadByPath);
+
+
 
 export default router;
