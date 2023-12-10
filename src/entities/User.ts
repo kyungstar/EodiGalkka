3a98e6acc27cdf7209bdef081332a8f55ad1c84a
kyungstar <azure0563@gmail.com>
2023-12-10 18:27:21 +0900

    User 설명 작업 추가

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -76,17 +76,20 @@ export class UserLogin {
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: [true, "User must type name"],
-        unique: true
+        type: String,               // 문자열 타입
+        required: [true, "User must type name"],   // 필수 속성, 빈 값이거나 없으면 에러
+        unique: true               // 중복 방지, 동일한 이름의 사용자가 없도록 함
     },
+    // 사용자 토큰
     token: {
-        type: String
+        type: String               // 문자열 타입
     },
+    // 온라인 상태 여부
     online: {
-        type: Boolean,
-        default: false
+        type: Boolean,             // 불리언 타입
+        default: false             // 기본값은 false, 사용자의 기본 온라인 상태는 오프라인
     }
 })
 
+// ChatUser 모델 생성 및 내보내기
 module.exports = mongoose.model("ChatUser", userSchema);
