881cd47584e32deadc91b437ecf774cfcba14ed3
kyungstar <azure0563@gmail.com>
2023-12-20 00:12:34 +0900

    나는 Raw SQL이 좋더라

--- src/modules/DBHelper.ts
+++ src/modules/DBHelper.ts
@@ -1,68 +1,348 @@
+import mariadb, {PoolCluster, PoolConnection} from "mariadb";
+import Logger from '../modules/Logger';
+import Config, {RUN_MODE} from "../../config";
+import {escape} from "sqlstring";
+import JSON from "json-bigint";
+import {EncryptColumns} from "../../config/Security"
 
-import { Repository } from 'typeorm';
-import {AppDataSource} from "./DBConfig";
-import {chattingRoom} from "../entities/Chat";
-import Logger from "./Logger";
-export default class DBHelper {
+interface DBConfig {
+    host: string;
+    port: number;
+    user: string;
+    password: string;
+    database: string;
+    multipleStatements: boolean;
+}
 
-    public static async getConnection(Repo: Function) {
-        const Repository = AppDataSource.getRepository(Repo);
-        return Repository;
+
+interface InsertObject {
+    [key: string]: string | number | boolean; // Adjust the types based on your requirements
+}
+
+interface TargetObject {
+    [key: string]: string | number | boolean; // Adjust the types based on your requirements
+}
+
+
+class DBHelper {
+    private cluster: PoolCluster;
+
+    constructor(Config: DBConfig) {
+        this.cluster = mariadb.createPoolCluster({
+            removeNodeErrorCount: 5,
+            restoreNodeTimeout: 5000
+        });
+
+        this.cluster.add('master', Config);
+
     }
 
-    public static async findOne(Repo: Function, whereObj: object) {
-
-        const Repository = await this.getConnection(Repo);
-
-        const targetObj = await Repository.findOneBy(whereObj);
-
-        Logger.info(JSON.stringify(targetObj) + "??");
-
-        await Repository.clear();
-
-        return targetObj;
+    public getCluster(): PoolCluster {
+        return this.cluster;
     }
 
-    public static async insert(Repo: Function, insertObj: object) {
-        const Repository = await this.getConnection(Repo);
-
-        await Repository.save(insertObj)
-
-        await Repository.clear();
-    }
-
-    public static async delete(Repo: Function, whereObj: object) {
-
+    public async getConnection(retry = 3): Promise<PoolConnection> {
         try {
-            const Repository = await this.getConnection(Repo);
+            const connection = this.getCluster();
 
-            await Repository.delete(whereObj)
-
-            await Repository.clear();
-
-            return true;
+            return connection.getConnection();
         } catch (err) {
-            Logger.error(err.stack);
-            return null;
-        }
-
-    }
-
-    public static async update(Repo: Function, updateObj: object, whereObj: object) {
-
-        try {
-            const Repository = await this.getConnection(Repo);
-
-            await Repository.update(updateObj, whereObj);
-
-            await Repository.clear();
-
-            return true;
-        } catch (err) {
-            Logger.error(err.stack);
-            return null;
+            Logger.debug(`${err} occurred`);
+            if (retry > 0) {
+                // Retry with a delay
+                await this.delay(1000); // 1-second delay
+                return this.getConnection(retry - 1);
+            } else {
+                return null;
+            }
         }
     }
+
+    private delay(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+
+    public async executeQuery(statement: string): Promise<PoolConnection> {
+        let conn = await this.getConnection();
+
+        try {
+            let result = await conn.query(statement.trim());
+
+            await conn.commit();
+            await conn.release();
+
+            return result;
+
+        } catch (err) {
+            Logger.debug('Query Select Fail', err);
+            await conn.release();
+            return null;
+        }
+    }
+
+    async tranQuery(statement: (string | object)[]) {
+
+        let conn: mariadb.PoolConnection = await this.getConnection();
+
+        try {
+
+            await conn.beginTransaction();
+
+            let query: string = statement.join(";");
+
+            let result = await conn.query(query);
+
+            const totalAffectedRows = result.reduce((sum: number, current: any) => sum + current.affectedRows, 0);
+
+            if(statement.length !== totalAffectedRows) {
+                throw new Error("Not Match AffectRows")
+            }
+
+
+            await conn.commit();
+            await conn.release();
+
+            return result;
+
+        } catch (err) {
+            await conn.rollback();
+            await conn.release();
+            Logger.debug('tranQuery Fail', err);
+        }
+    }
+
+    selectConditions(selectList: string[]): string {
+
+        let selectQuery = ``;
+
+        for (const targetObj of selectList) {
+            if (EncryptColumns.includes(targetObj))
+                selectQuery += `CAST(AES_DECRYPT(UNHEX(${targetObj}), ${escape(Config.DB[RUN_MODE].SECURITY.KEY)}) AS CHAR) as ${targetObj} ,`
+            else
+                selectQuery += `${targetObj} ,`
+
+        }
+
+        if(selectQuery.slice(-1) === `,`)
+            selectQuery = selectQuery.slice(0, -1);
+
+        return selectQuery;
+
+    }
+
+    whereConditions(whereObj: Record<string, any>): string {
+
+        let whereQuery = ` WHERE 1 = 1 `;
+
+        for (const targetObj in whereObj) {
+            if (EncryptColumns.includes(targetObj))
+                whereQuery +=  `AND CAST(AES_DECRYPT(UNHEX(${targetObj}), ${escape(Config.DB[RUN_MODE].SECURITY.KEY)}) AS CHAR) = ${escape(whereObj[targetObj])}`
+            else
+                whereQuery += ` AND ${targetObj} = ${escape(whereObj[targetObj])}`
+        }
+
+        return whereQuery;
+
+    }
+
+    findOne = async (tblName: string, whereObj?: any, selectList?: string[]): Promise<object> => {
+        try {
+
+            let selectQuery: string = `SELECT `;
+
+            if(selectList)
+                selectQuery += this.selectConditions(selectList);
+            else
+                selectQuery += ` * `
+
+            selectQuery += ` FROM ${tblName}`;
+
+            let whereQuery: string = ``;
+
+            if (whereObj)
+                whereQuery = this.whereConditions(whereObj);
+
+            const targetQuery = selectQuery + whereQuery;
+
+            Logger.debug('findOne Query is : ' + targetQuery);
+
+            const resultSet: any = await this.executeQuery(targetQuery);
+
+            if (!resultSet)
+                return null;
+
+            Logger.debug('findOne Query Result : ' + JSON.stringify(resultSet));
+
+            return resultSet[0];
+
+        } catch (err) {
+            Logger.debug('findOne Query Fail By : ' + err.stack);
+            return null;
+        }
+    };
+
+    find = async (tblName: string, whereObj?: any, selectList?: string[], addOption?: string): Promise<object> => {
+        try {
+
+            let selectQuery: string = `SELECT `;
+
+            if(selectList)
+                selectQuery += this.selectConditions(selectList);
+            else
+                selectQuery += ` * `
+
+            selectQuery += ` FROM ${tblName}`;
+
+            let whereQuery: string = ``;
+
+            if (whereObj)
+                whereQuery = this.whereConditions(whereObj);
+
+            const targetQuery = selectQuery + whereQuery + addOption;
+
+            Logger.debug('findOne Query is : ' + targetQuery);
+
+            const resultSet: any = await this.executeQuery(targetQuery);
+
+            if (!resultSet)
+                return null;
+
+            Logger.debug('findOne Query Result : ' + JSON.stringify(resultSet));
+
+            return resultSet;
+
+        } catch (err) {
+            Logger.debug('Query Is Occured By : ' + err.stack);
+            return null;
+        }
+    };
+
+    async Insert(tblName: string, insertObj: any): Promise<string | object> {
+        try {
+            const insertQuery: string = `INSERT INTO ${tblName} SET ${Object.entries(insertObj).map(([k, v]) => `${k} = ${(v as any)[0] === '\\' ? (v as any).slice(1) : escape(v)}`).join(', ')}`;
+
+            const insertResult: any = await this.executeQuery(insertQuery);
+
+            Logger.debug('Insert Query is : ' + insertQuery);
+
+            if(!insertResult.affectedRows)
+                return null;
+
+            Logger.debug('Insert Query Result : ' + JSON.stringify(insertResult));
+
+            return insertResult.insertId;
+
+        } catch (err) {
+            Logger.debug('Insert Query Fail By : ' + err.stack);
+            return null;
+        }
+    };
+
+    Delete = async (tblName: string, targetObj: TargetObject): Promise<string> => {
+        try {
+
+            const deleteQuery = `DELETE FROM ${tblName}`
+
+            const whereQuery = this.whereConditions(targetObj)
+
+            const targetQuery = deleteQuery + whereQuery;
+
+            Logger.debug('delete Query is : ' + targetQuery);
+
+            const resultSet: any = await this.executeQuery(targetQuery);
+
+            Logger.debug('delete Query Result : ' + JSON.stringify(resultSet));
+
+            if(!resultSet.affectedRows)
+                return null;
+
+            return resultSet;
+
+        } catch (err) {
+            Logger.debug('Query Select Fail');
+            return null;
+        }
+    };
+
+
+    Update = async (tblName: string, updateObj: any, targetObj: TargetObject): Promise<string> => {
+        try {
+
+            const selectQuery = `UPDATE ${tblName} SET ${Object.entries(updateObj).map(([k, v]) => `${k} = ${(v as any)[0] === '\\' ? (v as any).slice(1) : escape(v)}`).join(', ')}`
+
+            const whereQuery = `WHERE ${(this.whereConditions(targetObj))}`;
+
+            const targetQuery = selectQuery + whereQuery;
+
+            Logger.debug('update Query is : ' + targetQuery);
+
+            const resultSet: any = await this.executeQuery(targetQuery);
+
+            Logger.debug('update Query Result : ' + JSON.stringify(resultSet));
+
+            if(!resultSet.affectedRows)
+                return null;
+
+            return resultSet;
+
+        } catch (err) {
+            Logger.debug(err);
+            return null;
+        }
+    };
+
+    // ==== InsertQuery ====
+    InsertQuery(tblName: string, insertObj: any): string {
+        try {
+
+            let insertQuery = `INSERT INTO ${tblName} SET `;
+
+            let flag = true;
+            for (const targetObj in insertObj) {
+                if (EncryptColumns.includes(targetObj))
+                    insertQuery += `${flag ? `${targetObj} ` : `, ${targetObj}`}  = HEX(AES_ENCRYPT(${escape(insertObj[targetObj])}, ${escape(Config.DB[RUN_MODE].SECURITY.KEY)}))`
+                else
+                    insertQuery += ` ${flag ? `${targetObj} ` : `, ${targetObj}`} = ${escape(insertObj[targetObj])}`
+                flag = false;
+            }
+
+            return insertQuery;
+
+        } catch (err) {
+            Logger.debug('Query Insert Fail');
+            return null;
+        }
+    };
+
+    UpdateQuery = (tblName: string, updateObj: any, targetObj: TargetObject): string => {
+        try {
+
+            const selectQuery = `UPDATE ${tblName} SET ${Object.entries(updateObj).map(([k, v]) => `${k} = ${(v as any)[0] === '\\' ? (v as any).slice(1) : escape(v)}`).join(', ')}`
+
+            const whereQuery = `WHERE ${(this.whereConditions(targetObj))}`;
+
+            const targetQuery = selectQuery + whereQuery;
+
+            return targetQuery;
+
+        } catch (err) {
+            Logger.debug(err);
+            return null;
+        }
+    };
 
 
 }
+
+// Example usage
+const dbConfig: DBConfig = {
+    host: Config.DB[RUN_MODE].MYSQL.HOST,
+    port: Config.DB[RUN_MODE].MYSQL.PORT,
+    user: Config.DB[RUN_MODE].MYSQL.USER,
+    password: Config.DB[RUN_MODE].MYSQL.PASSWORD,
+    database: 'nodejs',
+    multipleStatements: true
+};
+
+export default new DBHelper(dbConfig);
