5068e3cd80df1490a9ca6e298ada2cc3c2f0f0aa
kyungstar <azure0563@gmail.com>
2023-12-19 21:04:39 +0900

    채팅 서비스 API 개설

--- src/routers/api/index.ts
+++ src/routers/api/index.ts
@@ -2,11 +2,15 @@ import {Router} from "express";
 import UserRoutes from "./UserRoutes";
 import FileRoutes from "./FileRoutes";
 import TourRoutes from "./TourRoutes";
+import ChatRoutes from "./ChatRoutes";
 
 const router = Router();
 
 // 고객 서비스
 router.use("/api/user", UserRoutes);
+
+// 채팅 서비스
+router.use("/api/chat", ChatRoutes);
 
 // 파일 서비스
 router.use("/api/file", FileRoutes);
