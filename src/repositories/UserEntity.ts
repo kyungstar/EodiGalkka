386dbb131d9ac92ddef744967c1ca5740f10aa02
kyungstar <azure0563@gmail.com>
2023-12-23 00:44:08 +0700

    UserEntity 작업

--- src/repositories/UserEntity.ts
+++ src/repositories/UserEntity.ts
@@ -1,7 +1,4 @@
 
-// 유저타입명시
-import crypto from "crypto-js";
-import Config, {RUN_MODE} from "../../config";
 
 export enum userType {
     USER = 'USER',
@@ -15,92 +12,46 @@ export enum genderType {
     FEMALE = 'FEMALE',
 }
 
-// 유저
-const userTypeSchema = {
-    type: 'string',
-    enum: Object.values(userType)
+const userClientSchema = {
+    default_schema: {
+        type: 'string',
+    },
+    phoneNumberSchema: {
+        type: 'string',
+        minLength: 8,
+        maxLength: 13,
+        pattern: '^\\d{3}-\\d{4}-\\d{4}$'
+    }, emailSchema: {
+        type: 'string',
+        minLength: 8,
+        maxLength: 45,
+        pattern: '[a-z0-9]+@[a-z]+\\.[a-z]{2,3}'
+    }, nickNameSchema: {
+        type: 'string',
+        minLength: 2,
+        maxLength: 6,
+    }, genderSchema: {
+        type: 'string',
+        enum: Object.values(genderType)
+    }, addressSchema: {
+        type: 'string',
+        maxLength: 45,
+    }, userTypeSchema: {
+        type: 'string',
+        enum: Object.values(userType)
+    }, loginIdSchema: {
+        type: 'string',
+        minLength: 8,
+        maxLength: 12
+    }
 };
 
 
-// 로그인아이디
-const loginIdSchema = {
-    type: 'string',
-    minLength: 8,
-    maxLength: 12
+
+
+export interface userInterface {
+    userId: string
 };
-
-// 비밀번호
-const pwdSchema = {
-    type: 'string'
-};
-
-// 이메일
-const emailSchema = {
-    type: 'string'
-};
-
-// 유저 아이디
-const userIdSchema = {
-    type: 'string',
-    minLength: 8,
-    maxLength: 50
-};
-
-// 고객명
-const nameSchema = {
-    type: 'string'
-};
-
-// 고객 닉네임
-const nickNameSchema = {
-    type: 'string',
-    minLength: 1,
-    maxLength: 6,
-};
-
-
-// 전화번호의 Format은 항상, 000-0000-000을 사용한다.
-const phoneNumberSchema = {
-    type: 'string',
-    minLength: 8,
-    maxLength: 13
-};
-
-const genderSchema = {
-    type: 'string',
-    enum: Object.values(genderType)
-};
-
-const addressSchema = {
-    type: 'string',
-    maxLength: 45,
-};
-
-const addressDetailSchema = {
-    type: 'string',
-    maxLength: 45,
-};
-
-
-// 회원가입
-export const userJoinSchema = {
-        type: 'object',
-        properties: {
-            userType: userTypeSchema,
-            loginId: loginIdSchema,
-            pwd: pwdSchema,
-            email: emailSchema,
-            name: nameSchema,
-            nickName: nickNameSchema,
-            phoneNumber: phoneNumberSchema,
-            gender: genderSchema,
-            address: addressSchema,
-            addressDetail: addressDetailSchema
-        },
-        required: ["userType", "loginId", "pwd", "email", "name", "gender"],
-};
-
-
 
 export interface userJoinInterface {
     userType: string,
@@ -120,8 +71,8 @@ export interface userJoinInterface {
 export const userLoginSchema = {
     type: 'object',
     properties: {
-        loginId: loginIdSchema,
-        pwd: pwdSchema,
+        loginId: userClientSchema.loginIdSchema,
+        pwd: userClientSchema.default_schema,
     },
     required: ["loginId", "pwd"],
 };
@@ -134,23 +85,40 @@ export interface userLoginInterface {
 export const userSchema = {
     type: 'object',
     properties: {
-        userId: userIdSchema
+        userId: userClientSchema.default_schema
     },
     required: ["userId"]
 };
 
 
-export interface userInterface {
-    userId: string
+
+// 회원가입
+export const userJoinSchema = {
+    type: 'object',
+    properties: {
+        userType: userClientSchema.userTypeSchema,
+        loginId: userClientSchema.loginIdSchema,
+        pwd: userClientSchema.default_schema,
+        email: userClientSchema.emailSchema,
+        name: userClientSchema.default_schema,
+        nickName: userClientSchema.nickNameSchema,
+        phoneNumber: userClientSchema.phoneNumberSchema,
+        gender: userClientSchema.genderSchema,
+        address: userClientSchema.addressSchema,
+        addressDetail: userClientSchema.addressSchema
+    },
+    required: ["userType", "loginId", "pwd", "email", "name", "gender"],
 };
 
 
+
+// 고객정보
 export const userInfoSchema = {
     type: 'object',
     properties: {
-        userId: userIdSchema,
-        phoneNumber: phoneNumberSchema,
-        email: emailSchema,
+        userId: userClientSchema.default_schema,
+        phoneNumber: userClientSchema.phoneNumberSchema,
+        email: userClientSchema.default_schema
     },
     required: ["userId"]
 };
