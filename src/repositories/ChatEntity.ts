f733046dbe6ccbcc89b9f9649baadde020707574
kyungstar <azure0563@gmail.com>
2023-12-29 00:27:46 +0900

    Chat Interface

--- src/repositories/ChatEntity.ts
+++ src/repositories/ChatEntity.ts
@@ -13,16 +13,28 @@ const chatSchema = {
 
 export interface chatRoomInterface {
     userId: string,
+    userIds: string,
     chatRoomName: string
+};
+
+export interface chatUserInterface {
+    userId: string
 };
 
 export const chatRoomSchema = {
     type: 'object',
     properties: {
         userId: chatSchema.defaultSchema,
+        userIds: chatSchema.defaultSchema,
         chatRoomName: chatSchema.chatRoomSchema
     },
     required: ["userId", "chatRoomName"]
 };
 
-
+export const chatUserSchema = {
+    type: 'object',
+    properties: {
+        userId: chatSchema.defaultSchema
+    },
+    required: ["userId"]
+};
